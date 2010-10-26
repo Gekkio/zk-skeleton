29bf2ed494702936833caabbec86bfd04262d866
Joonas Javanainen <joonas.javanainen@gmail.com>
2010-10-27 00:09:57 +0300

    Fix: Added ZK maven repository

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -19,6 +19,9 @@ class ZKSkeleton(info: ProjectInfo) extends DefaultWebProject(info) with JRebelW
   val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % Versions.jetty % "test"
   val localMaven = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
 
+  val zkConfig = ModuleConfiguration("org.zkoss.zk", zkRepo)
+  def zkRepo = "m2-zk" at "http://mavensync.zkoss.org/maven2"
+
   override def jettyContextPath = "/zkskeleton"
   override def jettyRunClasspath = super.jettyRunClasspath +++ ("lib_local") +++ ("lib_local" ** "*.jar")
   override def compileClasspath = super.compileClasspath --- (managedDependencyRootPath ** ("*-sources.jar" | "*-javadoc.jar"))
